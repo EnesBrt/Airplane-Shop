ea955b1d14699ddfe72ac700bcbf3d1bec8533fd
EnesBrt <enes.barut@gmx.com>
2024-04-09 17:14:36 +0200

    Update STATIC_ROOT setting in settings.py

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -125,11 +125,12 @@ USE_TZ = True
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if (
     not DEBUG
 ):  # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -138,4 +139,4 @@ if (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-PHONENUMBER_DEFAULT_REGION = 'FR'
+PHONENUMBER_DEFAULT_REGION = "FR"
