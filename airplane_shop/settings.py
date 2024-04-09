ca9bf3d70abf4958854dbe4106ef3f2cb8e3daef
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-04-09 15:01:30 +0200

    Model creation 🗂️

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+PHONENUMBER_DEFAULT_REGION = 'FR'
