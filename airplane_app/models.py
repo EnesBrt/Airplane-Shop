1a314db27734913d0d3e5e56f8842bec15d2e12e
EnesBrt <enes.barut@gmx.com>
2024-04-09 17:08:59 +0200

    Refactor models.py to use foreign key fields directly in OrderDetails

--- airplane_app/models.py
+++ airplane_app/models.py
@@ -15,7 +15,7 @@ class Orders(models.Model):
     id_client = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
     date_order = models.DateTimeField(auto_now=True)
     status_order = models.CharField()
-    id_transaction = models.CharField() #?
+    id_transaction = models.CharField()
 
 
 class Products(models.Model):
@@ -28,7 +28,7 @@ class Products(models.Model):
 
 
 class OrderDetails(models.Model):
-    id_order = models.ForeignKey("Order Id", to=Orders, on_delete=models.CASCADE)
-    id_product = models.ForeignKey("Product Id", to=Products, on_delete=models.CASCADE)
+    id_order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    id_product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.IntegerField("Quantity of ordered product", null=False)
     price = models.FloatField("Total price of the order", null=False)
