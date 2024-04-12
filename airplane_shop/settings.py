6e2eee62a2723a7edb74cf59d5a6cf8968ed2509
EnesBrt <enes.barut@gmx.com>
2024-04-12 17:08:22 +0200

    Update email settings in settings.py

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -201,3 +201,10 @@ HAYSTACK_CONNECTIONS = {
         "ENGINE": "haystack.backends.simple_backend.SimpleEngine",
     },
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "enesbarut99@gmail.com"
+EMAIL_HOST_PASSWORD = "ybir xmmc rjtu atnf"
