eef308c13cef7fafe8b9344c8237aaa49d85fae4
EnesBrt <enes.barut@gmx.com>
2024-04-15 17:38:32 +0200

    Update LANGUAGE_CODE setting in settings.py to "en-EN"

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -158,7 +158,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "fr-FR"
+LANGUAGE_CODE = "en-EN"
 
 TIME_ZONE = "UTC"
 
