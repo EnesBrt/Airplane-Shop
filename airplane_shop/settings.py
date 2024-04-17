663e54ca1096d15ebf59d7694f918e2886438cca
EnesBrt <enes.barut@gmx.com>
2024-04-17 17:20:19 +0200

    Update registration email settings in settings.py

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -204,6 +204,8 @@ HAYSTACK_CONNECTIONS = {
 
 OSCAR_DEFAULT_CURRENCY = "EUR"
 
+OSCAR_SEND_REGISTRATION_EMAIL = False
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
