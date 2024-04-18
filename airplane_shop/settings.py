f206b38e8a5bbb1a2e5dd1811956033d840bf856
EnesBrt <enes.barut@gmx.com>
2024-04-18 15:00:47 +0200

    Update DIRS path in settings.py to include airplane_shop/templates

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -104,7 +104,7 @@ ROOT_URLCONF = "airplane_shop.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(BASE_DIR, "airplane_shop/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
