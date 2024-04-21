d6dbb652ed2d7db364f014b843d41612701f48ad
EnesBrt <enes.barut@gmx.com>
2024-04-21 16:40:50 +0200

    Refactor customer/views.py to remove email verification bug and improve code readability

--- airplane_apps/customer/urls.py
+++ airplane_apps/customer/urls.py
@@ -2,10 +2,4 @@ from django.urls import include, path
 
 # from .views import ActivateAccountView
 
-# urlpatterns = [
-#     path(
-#         "activate/<uidb64>/<token>/",
-#         ActivateAccountView.as_view(),
-#         name="activate_account",
-#     ),
-# ]
+urlpatterns = []
