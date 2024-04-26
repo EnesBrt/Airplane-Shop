18402e32736ce94ac5c0a19670fab457e985615a
EnesBrt <enes.barut@gmx.com>
2024-04-26 13:48:18 +0200

    Update PayPal Express checkout URL in airplane_shop/urls.py

--- airplane_shop/urls.py
+++ airplane_shop/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(apps.get_app_config("oscar").urls[0])),
     path("customer/", include("airplane_apps.customer.urls")),
-    path("checkout/paypal/", include("paypal.express.urls")),
+    path(r"^checkout/paypal/", include("paypal.express.urls")),
 ]
