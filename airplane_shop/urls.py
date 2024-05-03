923002086d6d5863e6ced81541b6ac85686f5cb2
EnesBrt <enes.barut@gmx.com>
2024-05-03 16:23:10 +0200

    Update MEDIA_ROOT setting in settings.py and add static() function in urls.py

--- airplane_shop/urls.py
+++ airplane_shop/urls.py
@@ -18,6 +18,7 @@ from django.apps import apps
 from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
