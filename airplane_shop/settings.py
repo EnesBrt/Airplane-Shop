76292ed4648adcdeb11c54f1b401983af8f3887b
EnesBrt <enes.barut@gmx.com>
2024-06-09 19:14:01 +0200

    chore: Update image URLs to use static file paths

--- airplane_shop/settings.py
+++ airplane_shop/settings.py
@@ -18,7 +18,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -170,7 +169,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
